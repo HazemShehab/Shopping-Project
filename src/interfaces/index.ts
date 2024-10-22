66dd13045d91f1586215f119fd8cb6bf30816099
Hazem Shehab <hazemshehab6@gmail.com>
2024-10-22 14:04:35 +0300

    fix input name bug

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -13,7 +13,7 @@ export interface IProduct {
 
 export interface IFormInput {
     id: string,
-    name: string,
+    name: "title" | "descreption" | "imageURL" | "price";
     label: string,
     type: string
 }
